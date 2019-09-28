04860618e5f2159a3ecc11666e1a648fc7cb23ec
EdLo <b93303015@gmail.com>
2019-09-28 10:28:31 +0800

    [Mod] optimize algorithm

--- 977_Squares_of_a_Sorted_Array/main.go
+++ 977_Squares_of_a_Sorted_Array/main.go
@@ -1,41 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	input := []int{1, -4, 2, 3, -12, 0, 121, -8}
-	output := sortedSquares(input)
-
+	input := []int{-12, -8, -4, 0, 1, 8, 12, 121}
+	output := sortedSquares2(input)
 	fmt.Println(output)
 }
 
+/*
+Runtime: 500 ms, faster than 52.57% of Go online submissions for Squares of a Sorted Array.
+Memory Usage: 8.4 MB, less than 100.00% of Go online submissions for Squares of a Sorted Array.
+*/
 func sortedSquares(A []int) []int {
-	for i := 0; i < len(A); i++ {
-		A[i] *= A[i]
+	B := []int{}
+	for _, v := range A {
+		B = append(B, v*v)
 	}
-
-	return quickSort(A, 0, len(A)-1)
+	sort.Ints(B)
+	return B
 }
 
-func quickSort(A []int, front int, end int) []int {
-	if front < end {
-		pivot := partition(A, front, end)
-		quickSort(A, front, pivot-1)
-		quickSort(A, pivot+1, end)
-	}
-	return A
-}
-
-func partition(A []int, front int, end int) int {
-	pivot := A[end]
-	i := front - 1
-	for j := front; j < end; j++ {
-		if A[j] < pivot {
-			i++
-			A[i], A[j] = A[j], A[i]
+/*
+Runtime: 476 ms, faster than 96.42% of Go online submissions for Squares of a Sorted Array.
+Memory Usage: 8.3 MB, less than 100.00% of Go online submissions for Squares of a Sorted Array.
+*/
+func sortedSquares2(A []int) []int {
+	size := len(A)
+	res := make([]int, size)
+	for l, r, k := 0, size-1, size-1; l <= r; k-- {
+		if A[l]+A[r] < 0 {
+			res[k] = A[l] * A[l]
+			l++
+		} else {
+			res[k] = A[r] * A[r]
+			r--
 		}
 	}
-	i++
-	A[i], A[end] = A[end], A[i]
-	return i
+	return res
 }
