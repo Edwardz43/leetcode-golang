57db1940c4497d33737134a53601983450039b5f
Ed_Lo <ed_lo@mail.chungyo.net>
2020-06-12 17:35:42 +0800

    [MOD] Replaces Problem 1464 with better algorithm

--- 1464_Maximum_Product_of_Two_Elements_in_an_Array/solution/solution.go
+++ 1464_Maximum_Product_of_Two_Elements_in_an_Array/solution/solution.go
@@ -1,28 +1,27 @@
 package solution
 
-import "sort"
-
 /*
 	Given the array of integers nums,
 	you will choose two different indices i and j of that array.
 	Return the maximum value of (nums[i]-1)*(nums[j]-1).
 
 	Runtime: 4 ms, faster than 92.96% of Go online submissions for Maximum Product of Two Elements in an Array.
-	Memory Usage: 3 MB, less than 60.47% of Go online submissions for Maximum Product of Two Elements in an Array.
+	Memory Usage: 3 MB, less than 87.60% of Go online submissions for Maximum Product of Two Elements in an Array.
 */
 
 // MaxProduct return the maximum value
 func MaxProduct(nums []int) int {
-	var d data = nums
-	sort.Sort(d)
+	max1 := nums[0]
+	max2 := 0
 
-	return (d[0] - 1) * (d[1] - 1)
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > max1 {
+			max2 = max1
+			max1 = nums[i]
+		} else if nums[i] > max2 {
+			max2 = nums[i]
+		}
+	}
+
+	return (max1 - 1) * (max2 - 1)
 }
-
-type data []int
-
-func (d data) Len() int { return len(d) }
-
-func (d data) Less(i, j int) bool { return d[i] > d[j] }
-
-func (d data) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
