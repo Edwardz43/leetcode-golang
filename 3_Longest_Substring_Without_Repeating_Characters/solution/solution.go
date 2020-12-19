73ef0da31c810f7755350ba55ad04165a9d76769
edlo <b93303015@gmail.com>
2020-12-19 22:23:09 +0800

    #3 optimzed

--- 3_Longest_Substring_Without_Repeating_Characters/solution/solution.go
+++ 3_Longest_Substring_Without_Repeating_Characters/solution/solution.go
@@ -1,34 +1,25 @@
 package solution
 
+/*
+Runtime: 4 ms, faster than 88.05% of Go online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 2.7 MB, less than 76.99% of Go online submissions for Longest Substring Without Repeating Characters.
+*/
+
 // LengthOfLongestSubstring find the length of the longest substring without repeating characters.
 func LengthOfLongestSubstring(s string) int {
-	if len(s) == 0 || len(s) == 1 {
-		return len(s)
-	}
-	set := make(map[byte]bool, len(s))
-	max := 0
-	for i, j := 0, 1; i <= j && j < len(s); {
-		// fmt.Printf("i=%d, j=%d, i: %s, j: %s\n", i, j, string(s[i]), string(s[j]))
-		set[s[i]] = true
-		cursor := s[j]
-		if set[cursor] {
-			if j-i > max {
-				max = j - i
-				// fmt.Printf("max=%d\n", max)
-			}
-			set = make(map[byte]bool, 0)
-			i++
-			j = i + 1
-			continue
+	cache := [128]bool{}
+	max, lengh, cursor := 0, 0, 0
+	for i := range s {
+		for cache[s[i]] {
+			cache[s[cursor]] = false
+			lengh--
+			cursor++
 		}
-
-		if j-i+1 > max {
-			max = j - i + 1
-			// fmt.Printf("max=%d\n", max)
+		lengh++
+		cache[s[i]] = true
+		if lengh > max {
+			max = lengh
 		}
-		// fmt.Printf("max=%d\n", max)
-		set[cursor] = true
-		j++
 	}
 	return max
 }
