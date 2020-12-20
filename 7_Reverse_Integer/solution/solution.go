b85a8da48304472c386e2e81314016d6192dadf0
edlo <b93303015@gmail.com>
2020-12-20 18:04:41 +0800

    #7 adds V2 solution

--- 7_Reverse_Integer/solution/solution.go
+++ 7_Reverse_Integer/solution/solution.go
@@ -34,3 +34,39 @@ func Reverse(x int) int {
 	}
 	return r
 }
+
+/*
+Runtime: 4 ms, faster than 37.46% of Go online submissions for Reverse Integer.
+Memory Usage: 2.2 MB, less than 100.00% of Go online submissions for Reverse Integer.
+*/
+
+// ReverseV2 uses less memory but slower than original one.
+func ReverseV2(x int) int {
+	res := [10]int{}
+	negative := x < 0
+	if negative {
+		x *= -1
+	}
+	counter := 0
+	for x > 0 {
+		t := x % 10
+		res[counter] = t
+		x = x / 10
+		counter++
+	}
+	r := 0
+	for i := 0; i < counter; i++ {
+		r = r*10 + res[i]
+	}
+	if negative {
+		r *= -1
+		if r < 1<<31*-1 {
+			return 0
+		}
+		return r
+	}
+	if r > 1<<31-1 {
+		return 0
+	}
+	return r
+}
